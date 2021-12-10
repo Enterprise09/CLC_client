b579a768e47b005ce8ef7de0f414bf7b618ab6ce
Enterprise09 <goldenstaytes@gmail.com>
2021-12-10 15:42:09 +0900

    install sass - scss

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,8 @@
+import axios from "axios";
 import React from "react";
+import { Table } from "react-bootstrap";
 import "./Detail.css";
+import "../sass/Detail.scss";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -9,26 +12,63 @@ class Detail extends React.Component {
       history.push("/");
     }
   }
+
+  onSubmit = () => {
+    axios.post();
+  };
+
   //<span>{location.state.title}</span>;
   render() {
     const { location } = this.props;
     const { year, title, summary, poster, genres } = location.state;
     if (location.state) {
       return (
-        <div className="detail_box">
-          <img src={poster} />
-          <h2 className="title_box">{title}</h2>
-          <ul className="movie_genres">
-            {genres.map((genre, index) => (
-              <li key={index} className="li_genres">
-                {genre}
-              </li>
-            ))}
-          </ul>
-          <p>
-            <p className="summary_box"> {summary} </p>
-          </p>
-        </div>
+        <>
+          <div className="detail_box">
+            <img src={poster} />
+            <h2 className="title_box">{title}</h2>
+            <ul className="movie_genres">
+              {genres.map((genre, index) => (
+                <li key={index} className="li_genres">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p>
+              <p className="summary_box"> {summary} </p>
+            </p>
+            <table className="table">
+              <tr>
+                <th align="center">id</th>
+                <th align="center">title</th>
+                <th className="count" align="center">
+                  count
+                </th>
+              </tr>
+              <tr>
+                <td>ent</td>
+                <td>so funny!</td>
+                <td className="count">2</td>
+              </tr>
+              <tr>
+                <td>dkdkd</td>
+                <td>good for killing time</td>
+                <td className="count">5</td>
+              </tr>
+              <tr>
+                <td>jon</td>
+                <td>hello !!!</td>
+                <td className="count">1</td>
+              </tr>
+              <tr>
+                <td>jae</td>
+                <td>no time to die</td>
+                <td className="count">2</td>
+              </tr>
+            </table>
+            <a className="write">write</a>
+          </div>
+        </>
       );
     } else {
       return null;
